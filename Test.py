9e946636d043b0a13c69885d2ef3b45c6af53c9e
CPudding <150633243+CPuddingOwO@users.noreply.github.com>
2024-05-18 12:24:24 +0800

    :bug:  - Not Auto Change suffix to .apng on Animated Sticker & Emojis

--- Test.py
+++ Test.py
@@ -2,10 +2,10 @@ from led import *
 
 if __name__ == '__main__':
     # TmpURL = "https://store.line.me/emojishop/product/6309c424f1289d7e58de7523/zh-Hant"
-    # DownloadZIP(HTMLParser(URLParser(TmpURL, True)), DownloadDIR="./", ConvertAnime=True)
+    # DownloadZIP(HTMLParser(URLParser(TmpURL, True)), DownloadDIR="./", ConvertAnime=False)
     # Animation Emoji
     TmpURL = "https://store.line.me/emojishop/product/65c871dab6eca402da3ffa35/zh-Hant"
-    DownloadZIP(HTMLParser(URLParser(TmpURL, True)), DownloadDIR="./", ConvertAnime=True)
+    DownloadZIP(HTMLParser(URLParser(TmpURL, True)), DownloadDIR="./", ConvertAnime=False)
 
     # TmpURL = "https://store.line.me/stickershop/product/21802595/zh-Hant?qwqwqwq"
-    # DownloadZIP(HTMLParser(URLParser(TmpURL, True)))
+    # DownloadZIP(HTMLParser(URLParser(TmpURL, True)), DownloadDIR="./", ConvertAnime=False)
