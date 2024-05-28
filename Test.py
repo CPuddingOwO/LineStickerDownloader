d5008c175702c61ad1e9443d9e39e8ff2b4be11f
CPudding <150633243+CPuddingOwO@users.noreply.github.com>
2024-05-28 12:34:42 +0800

    :boom: Fix

--- Test.py
+++ Test.py
@@ -1,11 +1,18 @@
 from led import *
+from pathlib import Path
 
 if __name__ == '__main__':
+    print(f"Current directory: {Path.cwd()}")
+    _PathObject = Paths()
+    print(f"Saving Directory: {_PathObject.SaverPath}")
+    print(f"Tmp Directory: {_PathObject.TmpPath}")
+
+
     # TmpURL = "https://store.line.me/emojishop/product/6309c424f1289d7e58de7523/zh-Hant"
-    # DownloadZIP(HTMLParser(URLParser(TmpURL, True)), DownloadDIR="./", ConvertAnime=False)
+    # DownloadZIP(HTMLParser(URLParser(TmpURL, True)), _PathObject)
     # Animation Emoji
     TmpURL = "https://store.line.me/emojishop/product/65c871dab6eca402da3ffa35/zh-Hant"
-    DownloadZIP(HTMLParser(URLParser(TmpURL, True)), DownloadDIR="./", ConvertAnime=False)
+    DownloadZIP(HTMLParser(URLParser(TmpURL, True)), PathOptions=_PathObject, RewriteSuffix=".abcc", )
 
     # TmpURL = "https://store.line.me/stickershop/product/21802595/zh-Hant?qwqwqwq"
-    # DownloadZIP(HTMLParser(URLParser(TmpURL, True)), DownloadDIR="./", ConvertAnime=False)
+    # DownloadZIP(HTMLParser(URLParser(TmpURL, True)), _PathObject)
